fec9389a0b6bdeb4974ac412cb3b0e5cf05c350b
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-05-17 16:35:19 -0400

    chore: Update entity names and imports for PropertyAmenity and PropertyAddress

--- src/entity/property-amenity.entity.ts
+++ src/entity/property-amenity.entity.ts
@@ -1,10 +1,12 @@
-import { Entity, Column, ManyToMany } from 'typeorm';
+import { Entity, Column, ManyToMany, Index } from 'typeorm';
 import { AbstractEntity } from './abstract-entity';
 import { Property } from './property.entity';
 
 @Entity({ schema: 'kdo' })
-export class PropertyAmenity extends AbstractEntity {
-    @Column({ length: 255, unique: true })
+export class Amenity extends AbstractEntity {
+
+    @Index()
+    @Column({ length: 50, unique: true })
     name: string;
 
     @ManyToMany(() => Property, (property) => property.amenities)
