912186547ce2f953d5e3b585db833a82b25aa387
Vincent Adeyemi <vincent.adeyems@gmail.com>
2024-06-09 23:18:00 -0400

    chore: Add nullable property for `propertyIds` column in UserInvitation entity

--- src/entity/user.invitation.entity.ts
+++ src/entity/user.invitation.entity.ts
@@ -41,6 +41,6 @@ export class UserInvitation {
     @Column({ type: 'timestamp', nullable: true })
     acceptedAt?: string;
 
-    @Column({ type: 'simple-array' })
+    @Column({ type: 'simple-array', nullable: true })
     propertyIds?: string[]
 }
